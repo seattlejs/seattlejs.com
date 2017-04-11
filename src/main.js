5b9cb271bef5575d6549ad6880217862aa99ed51
Matt Hink <matt.hink@formidable.com>
2017-04-11 14:27:44 -0700

    We don't need no stinkin' Redux store yet.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from 'store';
 import App from './App';
 
 import 'materialize-css/dist/css/materialize.css';
 import 'core.css';
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
--- src/store/routes.js
+++
@@ -1,8 +0,0 @@
-export default {
-  '/': {},
-  '/login': {},
-  '/logout': {},
-  '/lobby': {},
-  '/settings': {},
-  '/g/:gameUuid': {},
-};
