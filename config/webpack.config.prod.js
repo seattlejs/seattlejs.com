078006a5bf3d0f56521b4c1ab23da2ad7cdae8f8
Matt Hink <matt.hink@formidable.com>
2017-04-19 16:46:14 -0700

    Use React production build.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -119,7 +119,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        'NODE_ENV': JSON.stringify('production'),
       }
     }),
     new HtmlWebpackPlugin({
