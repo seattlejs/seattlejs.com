c9e597176b41163d31ebaaa8d3fa15bec191878d
Carter Rabasa <carter.rabasa@gmail.com>
2022-05-11 13:15:38 -0700

    more copy + Jamund

--- src/views/index.js
+++ src/views/index.js
@@ -6,13 +6,15 @@ function futureTalks(talks) {
 }
 
 function Talk(talk) {
-    let { name, company, twitter, photo, title, abstract } = talk
+    let { name, company, twitter, photo, title, abstract, date } = talk
+    let d = new Date(date)
     return /*html*/`
         <div class="talk">
             <div class="photo"><img src="/images/speakers/${ photo }" alt="photo of ${ name }"/></div>
             <div class="details">
                 <h3>${ title }</h3>
                 <p>${ abstract }</p>
+                <p>Meetup: ${ new Intl.DateTimeFormat('en-US', {month: 'long'}).format(d) } ${ d.getFullYear() } </p>
                 <p>Speaker: ${ name }</p>
                 <p>Company: ${ company }</p>
                 <p>Twitter: <a target="_blank" href="https://twitter.com/${ twitter }">@${ twitter }</a></p>
@@ -24,12 +26,29 @@ function Talk(talk) {
 let Template = function({ talks }) {
     return /*html*/`
     <img src="/images/blank-seattlejs-header.png" alt="SeattleJS header image"/>
-    <h1>SeattleJS - a meetup for web developers in Seattle, WA</h1>
+    <h2>About Us</h2>
+    <p><img src="/images/seattlejs-march-2022.jpg" alt="SeattleJS March 2022 group photo"/></p>
+    <p>SeattleJS is the largest JavaScript meet up in Seattle. We hold our meet ups on the second Wednesday of every month.</p>
+    <p>SeattleJS exisits to inform, inspire, and encourage members of the JavaScript community. Our goal is for you to leave inspired and ready to innovate.</p>
+    <h2>Venue</h2>
+    <p>We meet at <a href="https://www.collectiveseattle.com">The Collective Seattle</a>, a co-working space and community in South Lake Union.</p>
+    <ul>
+        <li><a href="https://goo.gl/maps/rc11qrTiJXffBagC7">400 Dexter Ave N</a></li>
+        <li>Street parking is free after 6pm</li>
+    </ul>
+    <p>Members of SeattleJS can sign-up for a <a href="https://www.collectiveseattle.com/visitus">free Day Pass</a>. Feel free to bring your laptop for work or climbing shoes to hop on the bouldering wall - make a day of it!</p>
     <h2>Upcoming Talks</h2>
     ${ futureTalks(talks).map(Talk) }
-    <p>Join us for an <a href="https://ti.to/event-loop/">upcoming meetup</a>, everyone is welcome!</p>
-    <h2>Mailing List</h2>
-    <p>Stay up-to-date on upcoming meetups and other fun announcements by joining our <a href="/signup">community mailing list</a>.</p>
+    <h2>Attend</h2>
+    <p>RSVP for an <a href="https://ti.to/event-loop/">upcoming meetup</a>, everyone is welcome!</p>
+    <h2>Join our Community</h2>
+    <ul>
+        <li>Sign-up for our zero-spam <a href="/signup">community mailing list</a></li>
+        <li>Join us on <a href="https://discord.gg/HQdmgyWN58">Discord</a></li>
+        <li>Follow-up on <a href="https://twitter.com/cascadiajs">Twitter</a></li>
+    </ul>
+    <h2>Sponsor Us</h2>
+    <p>If your company would like to sponsor an upcoming meetup, please email us at <a href="mailto:info@cascadiajs.com">info@cascadiajs.com</a>.
     <h2>Code</h2>
     <p>The code for this website is hosted on <a href="https://github.com/seattlejs/seattlejs.com">Github</a>.</p>
 `
