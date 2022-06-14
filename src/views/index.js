7f9efb60ecbae0c8bf55619a124cea2b8c3b0797
Carter Rabasa <carter.rabasa@gmail.com>
2022-06-14 11:12:41 -0700

    added CFP

--- src/views/index.js
+++ src/views/index.js
@@ -30,17 +30,24 @@ let Template = function({ talks }) {
     <p><img src="/images/seattlejs-march-2022.jpg" alt="SeattleJS March 2022 group photo"/></p>
     <p>SeattleJS is the largest JavaScript meet up in Seattle. We hold our meet ups on the second Wednesday of every month.</p>
     <p>SeattleJS exisits to inform, inspire, and encourage members of the JavaScript community. Our goal is for you to leave inspired and ready to innovate.</p>
+    <h2>Upcoming Talks</h2>
+    ${ futureTalks(talks).length === 0 
+        ? '<p>There are no upcoming talks at the moment. Consider submitting an idea to our CFP!'
+        : futureTalks(talks).map(Talk) }
+    <h2>Attending</h2>
+    <p></p>
+    <p>RSVP for an <a target="_blank" href="https://ti.to/event-loop/">upcoming meetup</a>, everyone is welcome!</p>
+    <h2>Giving a Talk</h2>
+    <p>No matter who you are and what your experience level is, you can give a talk at SeattleJS. We accept everything from 5-10 lightning talks to ~30 minute presentations. If you've learned something new or have some hard-won widsom to share, you'll find SeattleJS a warm and welcoming place to speak.</p>
+    <p>Fill-out <a target="_blank" href="https://airtable.com/shrkvVTP37PnIqgoN">this form</a> and we'll be in touch!</p>
+    <p></p>
     <h2>Venue</h2>
-    <p>We meet at <a target="_blank" href="https://www.collectiveseattle.com">The Collective Seattle</a>, a co-working space and community in South Lake Union.</p>
+    <p>We typically meet at <a target="_blank" href="https://www.collectiveseattle.com">The Collective Seattle</a>, a co-working space and community in South Lake Union.</p>
     <ul>
         <li><a target="_blank" href="https://goo.gl/maps/rc11qrTiJXffBagC7">400 Dexter Ave N</a></li>
         <li>Street parking is free after 6pm</li>
     </ul>
     <p>Members of SeattleJS can sign-up for a <a target="_blank" href="https://www.collectiveseattle.com/visitus">free Day Pass</a>. Feel free to bring your laptop for work or climbing shoes to hop on the bouldering wall - make a day of it!</p>
-    <h2>Upcoming Talks</h2>
-    ${ futureTalks(talks).map(Talk) }
-    <h2>Attend</h2>
-    <p>RSVP for an <a target="_blank" href="https://ti.to/event-loop/">upcoming meetup</a>, everyone is welcome!</p>
     <h2>Join our Community</h2>
     <ul>
         <li>Sign-up for our zero-spam <a href="/signup">community mailing list</a></li>
@@ -57,7 +64,7 @@ let Template = function({ talks }) {
     <div id="organizers">
         <div class="organizer">
             <p><img src="/images/organizers/carter-rabasa.jpg" alt="photo of Carter Rabasa"/></p>
-            <p>Carter Rabasa<br/><a href="https://twitter.com/crtr0">@crtr0</a></p>
+            <p>Carter Rabasa<br/><a target="_blank" href="https://twitter.com/crtr0">@crtr0</a></p>
         </div>
         <div class="organizer">
             <p><img src="/images/organizers/ehtesh-choudhury.jpg" alt="photo of Ehtesh Choudhury"/></p>
