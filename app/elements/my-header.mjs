b87f38ecde581b6653f0d039ae23cd8056caa2f6
Andre Wiggins <andrewiggins@live.com>
2023-02-10 00:14:01 -0800

    Experiment with details element for mobile menu

--- app/elements/my-header.mjs
+++ app/elements/my-header.mjs
@@ -5,14 +5,19 @@ export default function MyHeader({ html }) {
         <div id="logo">
           <a href="/"><img src="/_public/images/logo.png" /></a>
         </div>
-        <div><a href="/about-us">About Us</a></div>
-        <div><a href="/speak">Give a Talk</a></div>
-        <div><a href="/join">Join Our Community</a></div>
-        <!--a href="/sponsoring">Sponsor Us</a-->
-        <div><a href="/code-of-conduct">Code of Conduct</a></div>
-        <div>
-          <a target="_blank" href="https://ti.to/event-loop/">Tickets</a>
-        </div>
+        <details>
+          <summary>Site navigation</summary>
+          <ul>
+            <li><a href="/about-us">About Us</a></li>
+            <li><a href="/speak">Give a Talk</a></li>
+            <li><a href="/join">Join Our Community</a></li>
+            <!--a href="/sponsoring">Sponsor Us</a-->
+            <li><a href="/code-of-conduct">Code of Conduct</a></li>
+            <li>
+              <a target="_blank" href="https://ti.to/event-loop/">Tickets</a>
+            </li>
+          </ul>
+        </details>
       </nav>
     </header>
   `
