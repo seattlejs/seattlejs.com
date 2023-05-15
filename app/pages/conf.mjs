c52fa403d6a8245c16bc986a3cf6db4868b540b1
Carter Rabasa <carter.rabasa@gmail.com>
2023-05-15 12:54:20 -0700

    added comment

--- app/pages/conf.mjs
+++ app/pages/conf.mjs
@@ -5,6 +5,7 @@ export default function ({ html, state = {} }) {
   return html`
     <my-layout>
       <div id="page">
+        <!-- no title needed -->
         <div class="page-body">
           <img src="/_public/images/seattlejsconf-2023-logo.png" alt="logo for SeattleJS Conf 2023"/>
         </div>
