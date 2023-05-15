6ebcb3e8ee1805ca0f526cb705c0ac65614f8871
Carter Rabasa <carter.rabasa@gmail.com>
2023-05-15 12:39:14 -0700

    added /conf page

---
+++ app/pages/conf.mjs
@@ -0,0 +1,14 @@
+export default function ({ html, state = {} }) {
+  let { store = {} } = state
+  let { events } = store
+  //console.log(events)
+  return html`
+    <my-layout>
+      <div id="page">
+        <div class="page-body">
+          <img src="/_public/images/seattlejsconf-2023-logo.png" alt="logo for SeattleJS Conf 2023"/>
+        </div>
+      </div>
+    </my-layout>
+  `
+}
