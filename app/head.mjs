717895b0da857aaec5e084f2391b4280b0aeb7a2
Carter Rabasa <carter.rabasa@gmail.com>
2023-05-19 12:11:36 -0700

    social sharing image

--- app/head.mjs
+++ app/head.mjs
@@ -14,7 +14,19 @@ export default function Head(state) {
         <link rel="stylesheet" href="/_public/styles/main.css">
         <link rel="icon" href="/_public/favicon.ico">
         <link rel="stylesheet" href="https://use.typekit.net/nln6hzq.css">
-        ${ path === '/conf' ? '<script src="https://js.tito.io/v2" async></script>' : ''}
+        ${ path === '/conf' 
+          ? `
+          <script src="https://js.tito.io/v2" async></script>
+          <meta name="image" property="og:image" content="https://seattlejs.com/_public/images/seattlejsconf-share.jpg" />
+          <meta name="url" property="og:url" content="https://seattlejs.com/conf" />
+          <meta name="type" property="og:type" content="website" />
+          <meta name="author" content="Carter Rabasa">
+          <meta property="og:title" content="SeattleJS Conf- August 8, 2023" />
+          <meta name="twitter:image" content="https://seattlejs.com/_public/images/seattlejsconf-share.jpg">
+          <meta name="twitter:card" content="summary_large_image">
+          <meta name="twitter:site" content="@seattlejs">
+          <meta name="twitter:title" content="SeattleJS Conf - August 8, 2023">
+        `: ''}
       </head>
     `
 }
