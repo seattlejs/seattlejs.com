3309c4f9a18b76e62967081ed4b35ce9197cd0b2
Carter Rabasa <carter.rabasa@gmail.com>
2023-06-15 08:22:13 -0700

    font/style tweaks, social sharing image

--- app/head.mjs
+++ app/head.mjs
@@ -25,7 +25,7 @@ export default function Head(state) {
         <meta name="description" content="SeattleJS is the largest JavaScript and web developer meetup in Seattle. Our meetings are the 2nd Wednesday of each month. We are an inclusive community and welcome everyone, including folks who are just getting started in tech.">
         <link rel="stylesheet" href="/_public/styles/main.css">
         <link rel="icon" href="/_public/favicon.ico">
-        <link rel="stylesheet" href="https://use.typekit.net/nln6hzq.css">
+        <link rel="stylesheet" href="https://use.typekit.net/smc6vzy.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css" integrity="sha512-rO+olRTkcf304DQBxSWxln8JXCzTHlKnIdnMUwYvQa9/Jd4cQaNkItIUj6Z4nvW1dqK0SKXLbn9h4KwZTNtAyw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         ${ path === '/conf' 
           ? `
