67cfddda6114b2031ab3567829ca6ac73caa5867
Carter Rabasa <carter.rabasa@gmail.com>
2023-07-28 12:13:25 -0700

    added video embed

--- app/elements/my-footer.mjs
+++ app/elements/my-footer.mjs
@@ -30,6 +30,7 @@ export default function MyFooter({ html }) {
     ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js" integrity="sha512-rdhY3cbXURo13l/WU9VlaRyaIYeJ/KBakckXIvJNAQde8DgpOmE+eZf7ha4vdqVjTtwQt69bD2wH2LXob/LB7Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>hljs.highlightAll();</script>
+    <script src="https://unpkg.com/@mux/mux-player"></script>
     <footer>
       <h2>Built with ❤️ in Seattle</h2>
       <p>
---
+++ app/pages/live.mjs
@@ -0,0 +1,30 @@
+export default function ({ html, state }) {
+  let { store = {} } = state
+  let { event = {} } = store
+  let playbackId = event.playbackId || 'v69RSHhFelSm4701snP22dYz2jICy4E4FUyk02rW4gxRM'
+  return html`
+    <style>
+    </style>
+    <my-layout>
+      <div id="page">
+        <div class="page-body">
+          <mux-player
+              stream-type="live"
+              playback-id="${ playbackId }"
+              metadata-video-title="SeattleJS Conf 2023 Livestream"
+              metadata-viewer-user-id="embed"
+              muted="true"
+              autoplay="any"
+              default-hidden-captions>
+          </mux-player>
+          <h2>Thanks to our Sponsors!</h2>
+          <section id="sponsors">
+            <view-sponsor tier="platinum" name="AWS" url="https://docs.amplify.aws/" image="aws.png"></view-sponsor>
+            <view-sponsor tier="gold" name="Courier" url="http://courier.com?utm_campaign=q2-fy23-conference-seattlejsconf&utm_source=conference&utm_medium=website" image="courier.png"></view-sponsor>
+            <view-sponsor tier="gold" name="Mux" url="http://mux.com" image="mux.png"></view-sponsor>
+          </section>
+        </div>
+      </div>
+    </my-layout>
+  `
+}
