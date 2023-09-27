0ab09c24123863553f8bb915b91070a621aed544
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-27 16:41:44 -0700

    talks rendering now

--- app/pages/talks.mjs
+++ app/pages/talks.mjs
@@ -1,13 +1,15 @@
 export default function ({ html, state = {} }) {
-  let { store = {} } = state
-  let { talks } = store
+  let { store } = state
+  let talks = [...Object.values(store)]
+
   return html`
     <my-layout>
       <div id="page">
         <div class="page-title"><h1>Talk List</h1></div>
         <div class="page-body">
+          <p> past talks
           <ul>
-            ${talks.map(t => /*html*/ `<li>${t.title}</li>`).join('')}
+            ${talks.map(t => /*html*/ `<li>${t.title} - Speaker: ${t.speaker_id.split('-').join(' ')}</li>`).join('')}
           </ul>
         </div>
       </div>
