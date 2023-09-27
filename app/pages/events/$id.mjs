d30bf6d296c59b06cd93e5c8c5553177859eaa5f
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-26 18:54:33 -0700

    feat: displaying data and starting html

--- app/pages/events/$id.mjs
+++ app/pages/events/$id.mjs
@@ -1,9 +1,41 @@
+import { marked } from "marked"
+
 export default function ({ html, state = {} }) {
   let { store = {} } = state
   let { event } = store
+
+  console.log('event: ', event)
   return html`
     <page-layout title=${event.title}>
       <pre>${JSON.stringify(event, null, 2)}</pre>
+      <h3>${event.title}</h3>
+      ${event.description && `<p>${marked(event.description)}</p>` }
+
+      ${event.sponsors.length > 0 ?
+        `<h4>Thanks to our Sponsors ❤️</h4>
+        ${event.sponsors
+          .map(
+            s =>
+              `<view-sponsor
+              image="${s.image}"
+              url="${s.url}"
+              copy="${s.copy}"
+              name="${s.name}"
+              tier="platinum"
+            ></view-sponsor>`
+          )
+          .join('')
+        }` : null
+      }
+      <div class="cta">
+        talks?
+      </div>
+
+      <div class="">
+        <a target="_self" href="/events">
+          See all past events <i class="fa-solid fa-arrow-right"></i>
+        </a>
+      </div>
     </page-layout>
   `
 }
