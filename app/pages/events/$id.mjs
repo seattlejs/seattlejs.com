1c31dae3d913192aa96fe6c13bbdb3aea1457ac5
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-27 16:43:43 -0700

    small change

--- app/pages/events/$id.mjs
+++ app/pages/events/$id.mjs
@@ -6,16 +6,12 @@ export default function ({ html, state = {} }) {
 
   return html`
     <page-layout title=${event.title}>
-      <pre>${JSON.stringify(event, null, 2)}</pre>
       <h3>${event.title}</h3>
       ${event.description && `<p>${marked(event.description)}</p>` }
 
       <h4>Thanks to our Sponsors ❤️</h4>
-      <event-sponsors ></event-sponsors>
-      <div class="cta">
-        talks?
-      </div>
-
+      ${event.sponsors.length > 0 ? html`<event-sponsors></event-sponsors>` : null}
+      <event-talks-list></event-talks-list>
       <div class="">
         <a target="_self" href="/events">
           See all past events <i class="fa-solid fa-arrow-right"></i>
