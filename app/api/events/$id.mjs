167e775c698440f405bdd5ed47efd16a98498b73
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-29 15:52:13 -0700

    normalize data and expand data in api events

--- app/api/events/$id.mjs
+++ app/api/events/$id.mjs
@@ -3,21 +3,36 @@ import sponsors from '../../data/sponsors.json' assert { type: 'json' }
 import talks from '../../data/talks.json' assert { type: 'json' }
 import speakers from '../../data/speakers.json' assert { type: 'json' }
 
+function inflateTalk(talk_id) {
+  let talk = talks.find(t => t.id === talk_id)
+  if (talk && talk.speaker_id) {
+    talk.speaker = speakers.find(s => s.id === talk.speaker_id)
+  }
+  return talk
+}
+
+function inflateSponsors(sponsor_id) {
+  return sponsors.find(s => s.id === sponsor_id)
+}
+
+function inflateEvent(event) {
+  if (event.talks) {
+    event.talks = event.talks.map(inflateTalk)
+    event.sponsors = event.sponsors.map(inflateSponsors)
+  }
+  return event
+}
+
 export async function get(req) {
-  let { id } = req.params
-
-  let event = events.find(s => s.id === id)
-  let eventSponsors = sponsors.filter(s => event.sponsors.includes(s.id))
-
-  let eventTalks = event.talks ?talks.filter(t => {
-    return event.talks.includes(t.id)
-  }) : []
-
-  let eventSpeakers = eventTalks ? speakers.filter(s => {
-    return eventTalks.map(t => t.speaker_id).includes(s.id)
-  }) : []
-
+  let inflatedEvents = events.map(inflateEvent)
+  let event = [inflatedEvents.find(e => e.id === req.params.id)]
+  let eventSponsors = event[0].sponsors
+  let eventTalks = event[0].talks
   return {
-    json: { event, eventSponsors, eventTalks, eventSpeakers }
+    json: {
+      events: event,
+      sponsors: eventSponsors,
+      talks: eventTalks
+    }
   }
 }
