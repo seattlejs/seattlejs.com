070f84148c7429123ccf0a2f861fc04fd6e2d1c3
Andre Wiggins <andrewiggins@live.com>
2024-08-06 15:43:22 -0700

    Embed Lu.ma into homepage

--- app/elements/list-events.mjs
+++ app/elements/list-events.mjs
@@ -2,7 +2,7 @@ export default function ListEvents({ html, state = {} }) {
   const { store = {} } = state
   const { events } = store
   if (events.length === 0) {
-    return html`<p>There are no upcoming events at the moment, hang tight!</p>`
+    return ''
   } else {
     return html`
       ${events.map(e => html` <view-event id="${e.id}"></view-event>`).join('')}
