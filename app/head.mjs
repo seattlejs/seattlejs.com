b9d1027e2e58e176d0f12745ceea7231451ca58a
Andre Wiggins <andrewiggins@live.com>
2024-08-22 14:21:59 -0700

    Fix favicon path

--- app/head.mjs
+++ app/head.mjs
@@ -23,7 +23,7 @@ export default function Head(state) {
         <title>${title ? title : `SeattleJS`}</title>
         <meta name="description" content="${description ? description : `SeattleJS is the largest JavaScript and web developer meetup in Seattle. Our meetings are the 2nd Wednesday of each month. We are an inclusive community and welcome everyone, including folks who are just getting started in tech.`}">
         <link rel="stylesheet" href="/_public/styles/main.css">
-        <link rel="shortcut icon" href="_public/favicon.jpg">
+        <link rel="shortcut icon" href="/_public/favicon.jpg">
         <link rel="stylesheet" href="https://use.typekit.net/smc6vzy.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css" integrity="sha512-rO+olRTkcf304DQBxSWxln8JXCzTHlKnIdnMUwYvQa9/Jd4cQaNkItIUj6Z4nvW1dqK0SKXLbn9h4KwZTNtAyw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         ${ sharing 
