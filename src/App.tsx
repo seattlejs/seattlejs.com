b41d904fbeb94d0484844ab9f0d27e640248e9ca
Andre Wiggins <andrewiggins@live.com>
2024-12-27 22:48:02 -0600

    Migrate all markdown pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,70 @@ import {
 } from 'preact-iso'
 
 import { NotFound } from './components/NotFound'
-import aboutUsUrl from './routes/about-us.md'
+import aboutUsPath from './routes/about-us.md'
+import codeOfConductPath from './routes/code-of-conduct.md'
+import confPromoInfoPath from './routes/conf/promo-info.md'
+import confSpeakerInfoPath from './routes/conf/speaker-info.md'
+import joinPath from './routes/join.md'
+import signupNextStepsPath from './routes/signup-next-steps.md'
+import speakPath from './routes/speak.md'
+import volunteerGuidePath from './routes/volunteer-guide.md'
+import volunteerPath from './routes/volunteer.md'
+import welcomePath from './routes/welcome.md'
+
 import { renderMarkdownRoute } from './utils/renderMarkdownRoute'
 
-const AboutUs = lazy(() => renderMarkdownRoute(aboutUsUrl))
+const routes = [
+  {
+    url: '/about-us',
+    component: lazy(() => renderMarkdownRoute(aboutUsPath)),
+  },
+  {
+    url: './conf/promo-info',
+    component: lazy(() => renderMarkdownRoute(confPromoInfoPath)),
+  },
+  {
+    url: '/conf/speaker-info',
+    component: lazy(() => renderMarkdownRoute(confSpeakerInfoPath)),
+  },
+  {
+    url: '/code-of-conduct',
+    component: lazy(() => renderMarkdownRoute(codeOfConductPath)),
+  },
+  {
+    url: '/join',
+    component: lazy(() => renderMarkdownRoute(joinPath)),
+  },
+  {
+    url: '/signup-next-steps',
+    component: lazy(() => renderMarkdownRoute(signupNextStepsPath)),
+  },
+  {
+    url: '/speak',
+    component: lazy(() => renderMarkdownRoute(speakPath)),
+  },
+  {
+    url: '/volunteer-guide',
+    component: lazy(() => renderMarkdownRoute(volunteerGuidePath)),
+  },
+  {
+    url: '/volunteer',
+    component: lazy(() => renderMarkdownRoute(volunteerPath)),
+  },
+  {
+    url: '/welcome',
+    component: lazy(() => renderMarkdownRoute(welcomePath)),
+  },
+]
 
 export default function App() {
   return (
     <LocationProvider>
       <ErrorBoundary>
         <Router>
-          <Route path="/about-us" component={AboutUs} />
+          {routes.map(({ url, component }) => (
+            <Route key={url} path={url} component={component} />
+          ))}
           <Route default component={NotFound} />
         </Router>
       </ErrorBoundary>
