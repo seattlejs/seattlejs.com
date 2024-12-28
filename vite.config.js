b7e4fd5b561189bbb9c456fbf1c798de731c4d21
Andre Wiggins <andrewiggins@live.com>
2024-12-27 18:26:03 -0600

    Setup hello world vite site

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/preact-starter/',
+  publicDir: 'public',
+  plugins: [preact()],
+})
