79163e1a8588a5d56615b26ddff8b78bac80b4c9
Andre Wiggins <andrewiggins@live.com>
2024-12-27 23:15:02 -0600

    Set the title when rendering markdown pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/seattlejs.com/',
   publicDir: 'public',
   plugins: [preact()],
   assetsInclude: ['**/*.md'],
